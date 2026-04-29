e372f223f55c8d584143a67fb54c5ddbe57c2e9e
ricardohorie07 <ricardohorie07@gmail.com>
2026-04-29 20:01:39 -0300

    exemplo de classe e programa orientado a objetos

--- src/Main.kt
+++ src/Main.kt
@@ -1,7 +1,23 @@
+class Pessoa(val nome: String, var idade: Int) { // exemplo de classe e programa orientado a objetos
+
+    fun fazerAniversario() {
+        idade++
+    }
+
+    fun apresentar() {
+        println("Meu nome é $nome e tenho $idade anos")
+    }
+}
+
 fun main() {
     println("Iniciando projeto de algoritmos em Kotlin")
 
     exemploLoop()
+
+    val p = Pessoa("Ricardo", 22)
+    p.apresentar()
+    p.fazerAniversario()
+    p.apresentar()
 }
 
 fun exemploLoop() {
@@ -9,3 +25,5 @@ fun exemploLoop() {
         println("i = $i")
     }
 }
+
+
